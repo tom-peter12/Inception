ce2bf343f1138b15bc8c61289ac43a2872cf67d0
tomas <tpetros@student.42abudhabi.ae>
2024-01-22 21:25:58 +0400

    DONE

--- srcs/requirements/wordpress/tools/setup_wp.sh
+++ srcs/requirements/wordpress/tools/setup_wp.sh
@@ -42,9 +42,15 @@ wp config create --allow-root --dbname=${WP_DB_NAME} --dbuser=${WP_USER} --dbpas
 echo "Installing WordPress deps..."
 wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWD} --admin_email=${WP_ADMIN_EMAIL}
 
+wp theme install hello-elementor --allow-root
+
+wp theme activate hello-elementor --allow-root
+
+wp core update
+
 echo "Creating users..."
 wp user create ${WP_ADMIN} ${WP_ADMIN_EMAIL} --role=administrator --user_pass=${WP_ADMIN_PASSWD} --path=/var/www/html/wordpress --allow-root
 
-wp user create ${WP_USER} tomaspetros@gmail.com --role=editor --user_pass=${WP_PASSWD} --path=/var/www/html/wordpress --allow-root 
+wp user create ${WP_USER} ${WP_USER_EMAIL} --role=editor --user_pass=${WP_PASSWD} --path=/var/www/html/wordpress --allow-root 
 
 exec "$@"
